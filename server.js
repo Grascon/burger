6883fe6329b0d99f1478e37203350f147963d08e
Grascon <gilberto.r.s@icloud.com>
2018-05-01 15:10:47 -0700

    Added add on

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 
 //using methodOverride to send POST request
 var methodOverride = require('method-override');
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
