eda620a3e889c896b1567f2508966b2d3a1dcb49
Grascon <gilberto.r.s@icloud.com>
2018-05-01 12:31:37 -0700

    Mysql connection update

--- config/connection.js
+++ config/connection.js
@@ -2,18 +2,13 @@
 var mysql = require("mysql");
 
 //connecting to mysql
-if (process.env.JAWSDB_URL){
-  var connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
-else {
-  connection = mysql.createConnection({
+var connection = mysql.createConnection({
     port: 3306,
     host: "localhost",
     user: "root",
     password: "Rascon1992",
     database: "burgers_db"
-  });
-};
+});
 
 connection.connect(function(err) {
   if (err) {
